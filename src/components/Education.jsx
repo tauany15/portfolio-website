beee6a04a6361c61ac53e8704c2e218577d4c5f3
tauany15 <58577704+tauany15@users.noreply.github.com>
2024-04-22 21:17:27 -0400

    update in the Education section
    Fixed footer typo.

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -7,28 +7,31 @@ function Education() {
         <div id="education" class="education-container">
             <section class="grid-education-item1">
                 <h3 class="grid-item-title">Studies</h3>
-                <p>Federal University of Minas Gerais - 08/2017</p>
+                <h4>Federal University of Minas Gerais - 08/2017</h4>
                 <p>Bachelor Degree: Information Systems</p>
-                <p>Udemy.com - 09/2023</p>
+                <h4>Udemy.com - 09/2023</h4>
                 <p>The Complete 2023 Web Development Bootcamp</p>
             </section>
 
             <section class="grid-education-item2">
                 <h3 class="grid-item-title">Skills</h3>
+                <div class="grid-items">
+
+                    <a><i class="fa-brands fa-js"></i>JavaScript</a>
+
+                    <a><i class="fa-brands fa-react"></i>React</a>
+
+                    <a><i class="fa-brands fa-html5"></i>HTML5</a>
 
 
-                <a><i class="fa-brands fa-js"></i> JavaScript</a>
+                    <a><i class="fa-brands fa-css3-alt"></i>CSS3</a>
 
-                <a><i class="fa-brands fa-react"></i>React</a>
+                    <a><i class="fa-brands fa-java"></i>Java</a>
 
-                <a><i class="fa-brands fa-html5"></i>HTML5</a>
+                    <a><i class="fa-solid fa-database"></i>SQL</a>
 
+                </div>
 
-                <a><i class="fa-brands fa-css3-alt"></i>CSS3</a>
-
-                <a><i class="fa-brands fa-java"></i>Java</a>
-
-                <a><i class="fa-solid fa-database"></i>SQL</a>
 
             </section>
 
